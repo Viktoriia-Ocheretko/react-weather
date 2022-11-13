bd4360ce301f4c821324745e8719e522f842c101
Viktoriia Ocheretko <110281588+Viktoriia-Ocheretko@users.noreply.github.com>
2022-11-13 15:56:41 +0100

    The UI change

--- src/App.js
+++ src/App.js
@@ -1,85 +1,88 @@
 import "./App.css";
 
 export default function App() {
+  let weatherData = {
+    city: "Kyiv",
+    date: "Today 16:02",
+    description: "Cloudy",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+    humidity: "77",
+    wind: "2.5",
+    temperature: "19",
+  };
+
   return (
     <div className="App">
-      <div className="container">
-        <div className="card">
-          <div className="card-body">
-            <div className="row">
-              <div className="col-9">
-                <form className="d-flex" role="search" id="search-form" />
-
-                <input
-                  className="form-control me-2"
-                  type="search"
-                  placeholder="Enter a city"
-                  aria-label="Search"
-                  id="city-input"
-                  autofocus="on"
-                />
-              </div>
-
-              <div className="col-1">
-                <button className="btn btn-outline-success" type="submit">
+      <div className="Weather">
+        <form className="search-form" id="form-search">
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Type a city.."
+                autofocus="on"
+                autocomplete="off"
+                className="form-control shadow-sm"
+                id="search-another"
+              />
+            </div>
+            <div className="col-3">
+              <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                <button className="btn btn-dark" type="submit">
                   Search
                 </button>
-              </div>
-
-              <div className="col-1">
                 <button
-                  type="button"
-                  class="btn btn-success"
-                  id="second-button"
+                  className="btn btn-outline-dark"
+                  id="current-button"
+                  type="submit"
                 >
                   Current
                 </button>
               </div>
             </div>
-
-            <div className="row cities-data">
-              <div className="col-6 chosen-city" id="city-of-choise">
-                New York
-              </div>
-              <div className="col-6 weather-conditions"></div>
-              <div className="col-6" id="data-now">
-                Tuesday 10:00
-              </div>
-              <div className="col-6">
-                Wind: <span id="wind"></span> 5<span>km/h</span>
-              </div>
-              <div className="col-6" id="description">
-                Clouds
-              </div>
-              <div className="col-6">
-                Humidity: <span id="humidity"></span> 80<span>%</span>
-              </div>
-              <div className="col-6 current-temperature">
-                <img
-                  src="https://ssl.gstatic.com/onebox/weather/64/rain.png"
-                  alt="Clear"
-                  id="icon"
-                  class="float-left"
-                />
-                <span id="temperature-data"> 23</span>
-                <span id="celsius">
-                  <a href="#" id="celsius-link">
-                    °C
-                  </a>
-                </span>
-                <span className="separator">|</span>
-                <span>
-                  <a href="#" id="fahrenheit-link">
-                    °F
-                  </a>
-                </span>
-              </div>
-              <div className="col-6"></div>
-            </div>
-
-            <div className="weather-forecast" id="forecast"></div>
           </div>
+        </form>
+        <h1> {weatherData.city} </h1>
+        <ul>
+          <li>
+            {weatherData.date}
+            <span id="date"></span>
+          </li>
+          <li id="description"></li>
+        </ul>
+        <div className="row">
+          <div className="col-6">
+            <div className="d-flex wheather-temperature">
+              <img
+                src={weatherData.imgUrl}
+                alt="Cloudy icon"
+                className="weather-icon float-left"
+                id="icon"
+              />
+              <div className="d-flex wheather-temperature">
+                <span className="temperature">{weatherData.temperature}</span>
+                <span className="units">
+                  <a href="/" id="celsius-link">
+                    C°
+                  </a>
+                </span>
+              </div>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li> Humidity: {weatherData.humidity}%</li>
+
+                <li>Wind speed: {weatherData.wind}</li>
+              </ul>
+              <hr />
+            </div>
+          </div>
+          <div className="'weather-forecast" id="forecast"></div>
         </div>
+      </div>
+      <div className="gitlink">
+        <a href="gg">Open-source code </a>
+        by hh
       </div>
     </div>
   );
